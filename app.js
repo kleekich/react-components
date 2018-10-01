6e4d6d78c61fa80d4e5d90f0d494e7b5953331d4
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-01 11:56:19 -0700

    Implements GorceryList with React Components
    Implement GroceryList, GroceryItemKale, GroceryItemCucumber

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+
+import ReactDOM from 'react-dom';
+
+var App = () => (
+	<div id="app">Some cliche salutation</div>
+);
+
+var GroceryList = () => (
+	<ul>
+		<GroceryItemKale />
+		<GroceryItemCucumber />
+	</ul>
+
+);
+
+var GroceryItemKale = () => {
+	<li>Kale</li>
+}
+
+var GroceryItemCucumber = () => {
+	<li>Cucumber</li>
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
